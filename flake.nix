9d7ce7f2503741be63a715d404f45742a18b3ddd
Oleg Pykhalov <go.wigust@gmail.com>
2021-06-30 17:23:45 +0300

    flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     deploy-rs.url = "github:serokell/deploy-rs";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     flake-utils.url = "github:numtide/flake-utils";
-    majordomo.url = "git+ssh://git@github.com/mjuh/ci-nixpkgs";
+    majordomo.url = "git+ssh://git@github.com/mjuh/ci-nixpkgs?ref=ispconfig";
   };
 
   outputs = { self, flake-utils, nixpkgs, majordomo, deploy-rs, ... } @ inputs:
